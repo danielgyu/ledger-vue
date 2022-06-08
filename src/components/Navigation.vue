c428c0ed2ff3574fa62bcd7fa512a41220970a22
kungyu <hismayfly@gmail.com>
2022-06-08 23:41:05 +0900

    [js/vue/ledger] multiword

--- src/components/Navigation.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div id="header"></div>
-  <div id="container">
-      <div id="payments">
-          <router-link class="nav-link" :to="{name: 'Payment'}">Payments</router-link>
-      </div>
-  </div>
-</template>

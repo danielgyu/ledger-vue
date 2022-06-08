5325104fa177fc2de956d69d2151a19cb72a1ee9
kungyu <hismayfly@gmail.com>
2022-06-08 23:39:33 +0900

    [js/vue/ledger] setting up

---
+++ src/components/Navigation.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="header"></div>
+  <div id="container">
+      <div id="payments">
+          <router-link class="nav-link" :to="{name: 'Payment'}">Payments</router-link>
+      </div>
+  </div>
+</template>
---
+++ src/router/index.js
@@ -0,0 +1,13 @@
+import { createRouter } from "vue-router"
+
+const routes = [
+
+]
+
+const router = createRouter({
+    routes
+})
+
+export default {
+    router
+}
